ce581c793b18c3f006a5dc5d1cfb2d6161620df8
Mykola <95630245+miowff@users.noreply.github.com>
2024-02-13 09:30:45 +0200

    album display fix

--- src/components/Albums/Album/Album.tsx
+++ src/components/Albums/Album/Album.tsx
@@ -1,6 +1,7 @@
 import { AlbumModel } from "@/models/album";
 import AlbumIcon from "../../../public/album.svg";
 import { useNavigate } from "react-router-dom";
+import { format } from "date-fns";
 interface Props {
   album: AlbumModel;
 }
@@ -14,13 +15,15 @@ export const Album = ({ album }: Props) => {
       }}
     >
       <div className="album__inner">
-        <img className="album__icon" src={AlbumIcon} />
-        <div className="album__info">
-          <h1 className="album__title">{album.name}</h1>
-          <p className="album__location">{album.location}</p>
-          <p className="album__date">{album.date}</p>
+        <div className="album__content">
+          <img className="album__icon" src={AlbumIcon} />
+          <div className="album__info">
+            <h1 className="album__title">{album.name}</h1>
+            <p className="album__location">{album.location}</p>
+          </div>
         </div>
       </div>
+      <p className="album__date">{format(album.date, "yyyy-MM-dd")}</p>
     </div>
   );
 };
